7893bdd23ad6b196e0b5c6e1fa31e501166969bd
Gustavo Silveira Silva <silva.gustavo@hotmail.com.br>
2024-12-11 19:28:48 -0300

    exercicio

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log("Olá mundo");
+// No nosso dia a dia, o uso de senhas é algo comum, pois usamos muitos sistemas ou aplicativos diferentes.
+// Como recomendação de segurança, é necessário que para cada aplicativo que usamos, seja gerada uma senha segura.
+// Usando a técnica de arrays
+// gere 5 senhas que contenham pelo menos
+// 10 caracteres, contendo pelo menos 5 letras, sendo 2 maiúsculas e 2 minúsculas, 2 números e um caractere especial (ponto, hashtag, barra, ponto de interrogação, arroba).
+// Use a tabela ASCII como base para gerar caracteres aleatórios.
